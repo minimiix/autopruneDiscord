6bb663113c7b378f4f2ee3a2a16e79bf78ce08c8
Tyler Mammone <tyler@tylermammone.com>
2020-05-14 18:33:27 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 // ---------------------------------------------------
 // Discord autoprune channels
-// See it in action @ https://discord.gg/XGvHW5D
+// See it in action @ https://discord.gg/4ByYrAV
 // https://github.com/tylermammone/disappearingDiscord
 // ---------------------------------------------------
 // Settings
@@ -9,33 +9,39 @@
 const privateBotToken = BOT_TOKEN;
 
 const autoPrune = {
-    '699902897319837779':'25', // #general 25 lines (make sure you use your own channel ID number)
-    '699954259323650140':'15' // #private 15 lines (make sure you use your own channel ID number)
+  "699902897319837779": "25", // #general 25 lines (make sure you use your own channel ID number)
+  "699954259323650140": "15", // #private 15 lines (make sure you use your own channel ID number)
 };
 
 // ---------------------------------------------------
 // System
 // ---
 
-const discord = require('discord.js');
+const discord = require("discord.js");
 const client = new discord.Client();
 
 client.login(process.env.privateBotToken);
 
-client.on('ready', async () => {
-    console.log('Ready as <@'+client.user.id+'>!');
+client.on("ready", async () => {
+  console.log("Ready as <@" + client.user.id + ">!");
 });
 
-client.on('message', (message) => {
-    if(autoPrune[message.channel.id]){
-        var pruneAt = parseInt(autoPrune[message.channel.id])+10;
-        message.channel.messages.fetch({ limit: pruneAt }).then(messages => {
-            let previousMessages = messages.array();
-            for(let i = 0; i < previousMessages.length; i++){
-                if(i>=parseInt(autoPrune[message.channel.id])){
-                    message.channel.messages.fetch(previousMessages[i].id).then(msg => msg.delete()).catch(console.error);
-                }
-            }          
-        }).catch(console.error);
-    }
+client.on("message", (message) => {
+  if (autoPrune[message.channel.id]) {
+    var pruneAt = parseInt(autoPrune[message.channel.id]) + 10;
+    message.channel.messages
+      .fetch({ limit: pruneAt })
+      .then((messages) => {
+        let previousMessages = messages.array();
+        for (let i = 0; i < previousMessages.length; i++) {
+          if (i >= parseInt(autoPrune[message.channel.id])) {
+            message.channel.messages
+              .fetch(previousMessages[i].id)
+              .then((msg) => msg.delete())
+              .catch(console.error);
+          }
+        }
+      })
+      .catch(console.error);
+  }
 });
